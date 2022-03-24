6c2010f330e6d2e521634f98565fd8a0a7032510
Никита Киселев <nikitakiselev@Nikitas-MacBook-Air.local>
2022-03-24 19:59:19 +0500

    fix

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from time import sleep
 import json
 from bot_api import TG_bot
 
-# start: 09.30 - 12.00 (bot is working)
+# start: 09.30 - 12.00 (tg_api is working)
 # start: 15.00 - 18.00 (fb_api is working)
-# start: 19.00 - __.00 (fb_api is working)
+# start: 19.00 - 20.00 (docker is working)
 
 try:
     with open("credentials.json", "r") as cr:
